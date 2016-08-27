831dc6bee212bb47168dc7cec846bb5f4825f870
Amitrajit Sarkar <aaiijmrtt@gmail.com>
2016-08-27 14:30:52 +0530

    fix: mfcc.py

--- code/mfcc.py
+++ code/mfcc.py
@@ -2,7 +2,7 @@ import librosa as lr
 
 def audio2MFCC(filein, config):
 	y, sr = lr.load(filein)
-	return lr.feature.mfcc(y = y, sr = sr, n_mfcc = config.getint('n_mfcc'), hop_length = config.getint('hop_length'))
+	return lr.feature.mfcc(y = y, sr = sr, n_mfcc = config.getint('mfcc', 'n_mfcc'), hop_length = config.getint('mfcc', 'hop_length'))
 
 def MFCC2delta(mfcc):
 	return lr.feature.delta(mfcc)
