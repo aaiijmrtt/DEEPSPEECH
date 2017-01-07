c4a61625e3223a0de5d5e5d334477d040d3168fe
Amitrajit Sarkar <aaiijmrtt@gmail.com>
2017-01-08 00:24:41 +0530

    adding speaker recognition

--- code/dsi.py
+++ code/dsi.py
@@ -1,6 +1,6 @@
 import configparser, sys, os
 import tensorflow as tf, numpy as np
-import fnn, rnn, cnn, ce, util
+import fnn, rnn, cnn, ce, speakerutil
 
 def dsi(config):
 	model = dict()
@@ -10,9 +10,10 @@ def dsi(config):
 	model = fnn.fnn(model, config, 'fnn', 'rnn')
 	model = ce.ce(model, config, 'ce', 'fnn')
 
+	model['loss'] = model['ce_loss']
 	model['step'] = tf.Variable(0, trainable = False, name = 'step')
 	model['lrate'] = tf.train.exponential_decay(config.getfloat('global', 'lrate'), model['step'], config.getint('global', 'dstep'), config.getfloat('global', 'drate'), staircase = False, name = 'lrate')
-	model['optim'] = getattr(tf.train, config.get('global', 'optim'))(model['lrate']).minimize(model['ce_loss'], global_step = model['step'], name = 'optim')
+	model['optim'] = getattr(tf.train, config.get('global', 'optim'))(model['lrate']).minimize(model['loss'], global_step = model['step'], name = 'optim')
 
 	return model
 
@@ -25,9 +26,10 @@ if __name__ == '__main__':
 	config = configparser.ConfigParser(interpolation = configparser.ExtendedInterpolation())
 	config.read(sys.argv[1])
 	model = dsi(config)
+	speakers = speakerutil.speakermap(sys.argv[2])
 
 	with tf.Session() as sess:
 		sess.run(tf.initialize_all_variables())
-		util.train(model, sess, config, sys.argv[2], feed)
-		print util.test(model, sess, config, sys.argv[2], feed)
+		speakerutil.train(model, sess, config, sys.argv[3], feed, speakers)
+		print speakerutil.test(model, sess, config, sys.argv[4], feed, speakers)
 		tf.train.Saver().save(sess, os.path.join(config.get('global', 'path'), 'model'))
---
+++ code/speakerutil.py
@@ -0,0 +1,64 @@
+import os
+import numpy as np
+
+def prep(config, dirname):
+	mfcc_size = config.getint('mfcc', 'n_mfcc')
+
+	for dirpath, dirnames, filenames in os.walk(dirname):
+		for filename in filenames:
+			if filename.endswith('.flac'):
+				_file = os.path.join(dirpath, filename)
+				_mfcc = mfcc.audio2MFCC(_file, config)
+				_delta = mfcc.MFCC2delta(_mfcc)
+				_augmented = np.vstack([_mfcc, _delta])
+				np.savetxt('%s_%i.mfcc' %(_file[: -5], mfcc_size), _augmented)
+
+def speakermap(dirname):
+	speakers, skip = dict(), 12
+	for i, line in enumerate(open(os.path.join(dirname, 'SPEAKERS.TXT'))):
+		if i < skip: continue
+		speakers[int(line[: 4])] = i - skip
+	return speakers
+
+def train(model, sess, config, dirname, feed, speakermap):
+	features, labelslen, labelsind, labelsval = list(), list(), list(), list()
+	batch_size, time_size = config.getint('global', 'batch_size'), config.getint('global', 'time_size')
+
+	for dirpath, dirnames, filenames in os.walk(dirname):
+		for filename in filenames:
+			if filename.endswith('.txt'):
+				for line in open(os.path.join(dirpath, filename)):
+					labels = [speakermap[int(line.split(' ', 1)[0].split('-')[0])]]
+					lenfeatures, lenlabels = len(features), len(labels)
+					labelslen.append(lenlabels)
+					labelsind.extend([[lenfeatures, i] for i in xrange(lenlabels)])
+					labelsval.extend(labels)
+					features.append(np.loadtxt(os.path.join(dirpath, '%s_%i.mfcc' %(line.split()[0], config.getint('mfcc', 'n_mfcc'))))[:, :10])
+
+					if lenfeatures + 1 == batch_size:
+						sess.run([model['optim']], feed_dict = feed(features, labelslen, labelsind, labelsval, batch_size, time_size))
+						features, labelslen, labelsind, labelsval = list(), list(), list(), list()
+
+def test(model, sess, config, dirname, feed, speakermap):
+	features, labelslen, labelsind, labelsval = list(), list(), list(), list()
+	batch_size, time_size, mfcc_size = config.getint('global', 'batch_size'), config.getint('global', 'time_size'), config.getint('mfcc', 'n_mfcc')
+	total_loss, inf_count = 0.0, 0
+
+	for dirpath, dirnames, filenames in os.walk(dirname):
+		for filename in filenames:
+			if filename.endswith('.txt'):
+				for line in open(os.path.join(dirpath, filename)):
+					labels = [speakermap[int(line.split(' ', 1)[0].split('-')[0])]]
+					lenfeatures, lenlabels = len(features), len(labels)
+					labelslen.append(lenlabels)
+					labelsind.extend([[lenfeatures, i] for i in xrange(lenlabels)])
+					labelsval.extend(labels)
+					features.append(np.loadtxt(os.path.join(dirpath, '%s_%i.mfcc' %(line.split()[0], config.getint('mfcc', 'n_mfcc'))))[:, :10])
+
+					if lenfeatures + 1 == batch_size:
+						loss = sess.run(model['loss'], feed_dict = feed(features, labelslen, labelsind, labelsval, batch_size, time_size))
+						if loss == float('inf'): inf_count += 1
+						else: total_loss += loss
+						features, labelslen, labelsind, labelsval = list(), list(), list(), list()
+
+	return total_loss, inf_count
