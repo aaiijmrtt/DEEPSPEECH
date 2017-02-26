6eff1b88efa7e954d96e92e6d0cd6f9b1e304d84
Amitrajit Sarkar <aaiijmrtt@gmail.com>
2017-02-27 00:29:03 +0530

    tensorflow 1.0 upgrade: ctc

--- code/ctc.py
+++ code/ctc.py
@@ -7,7 +7,7 @@ def ctc(model, config, scope, connect):
 			model['%s_in1length' %scope] = model['%s_out1length' %connect]
 			model['%s_in2length' %scope] = model['%s_out2length' %connect]
 			model['%s_maxin2length' %scope] = model['%s_maxout2length' %connect]
-			model['%s_inputs' %scope] = tf.unpack(model['%s_outputs' %connect], model['%s_maxin2length' %scope], 0, '%s_inputs' %scope)
+			model['%s_inputs' %scope] = tf.unstack(model['%s_outputs' %connect], model['%s_maxin2length' %scope], 0, '%s_inputs' %scope)
 			model['%s_out0length' %scope] = model['%s_in0length' %connect]
 			model['%s_out1length' %scope] = model['%s_in1length' %connect]
 			model['%s_out2length' %scope] = tf.placeholder(tf.int32, [model['%s_in0length' %scope]], '%s_out2length' %scope)
@@ -22,7 +22,7 @@ def ctc(model, config, scope, connect):
 			model['%s_labels' %scope] = tf.SparseTensor(model['%s_labels_ind' %scope], model['%s_labels_val' %scope], [model['%s_maxin2length' %scope], model['%s_in0length' %scope]])
 
 		with tf.variable_scope('loss'), tf.name_scope('loss'):
-			model['%s_loss' %scope] = tf.reduce_sum(tf.nn.ctc_loss(model['%s_inputs' %scope], model['%s_labels' %scope], model['%s_labels_len' %scope]), name = '%s_loss' %scope)
+			model['%s_loss' %scope] = tf.reduce_sum(tf.nn.ctc_loss(model['%s_labels' %scope], model['%s_inputs' %scope], model['%s_labels_len' %scope]), name = '%s_loss' %scope)
 
 		with tf.variable_scope('outputs'), tf.name_scope('outputs'):
 			model['%s_output' %scope] = tf.nn.ctc_beam_search_decoder(model['%s_inputs' %scope], model['%s_out2length' %scope], model['%s_beamwidth' %scope], model['%s_toppaths' %scope])
