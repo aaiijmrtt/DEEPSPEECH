5e8b835df7e85badcf6802b76546576558228430
Amitrajit Sarkar <aaiijmrtt@gmail.com>
2017-03-02 00:30:06 +0530

    tensorflow 1.0 upgrade: rnn

--- code/rnn.py
+++ code/rnn.py
@@ -14,39 +14,39 @@ def rnn(model, config, scope, connect = None):
 				model['%s_in1length' %scope] = model['%s_out1length' %connect]
 				model['%s_in2length' %scope] = model['%s_out2length' %connect]
 				model['%s_maxin2length' %scope] = model['%s_maxout2length' %connect]
-				model['%s_inputs' %scope] = tf.unpack(model['%s_outputs' %connect], model['%s_maxin2length' %scope], 0, '%s_inputs' %scope)
+				model['%s_inputs' %scope] = tf.unstack(model['%s_outputs' %connect], model['%s_maxin2length' %scope], 0, '%s_inputs' %scope)
 			model['%s_out0length' %scope] = model['%s_in0length' %scope]
 			model['%s_out1length' %scope] = config.getint(scope, 'state_size') if config.has_option(scope, 'state_size') else model['%s_in1length' %scope]
 			model['%s_out2length' %scope] = model['%s_in2length' %scope]
 			model['%s_maxout2length' %scope] = model['%s_maxin2length' %scope]
 
 		with tf.variable_scope('cells'), tf.name_scope('cells'):
-			if config.get(scope, 'link_type') == 'rnn':
-				model['%s_rnn' %scope] = getattr(tf.nn.rnn_cell, config.get(scope, 'cell_type'))(model['%s_out1length' %scope], state_is_tuple = True)
-			elif config.get(scope, 'link_type') == 'bidirectional_rnn':
-				model['%s_rnn_fw' %scope] = getattr(tf.nn.rnn_cell, config.get(scope, 'cell_type'))(model['%s_out1length' %scope], state_is_tuple = True)
-				model['%s_rnn_bw' %scope] = getattr(tf.nn.rnn_cell, config.get(scope, 'cell_type'))(model['%s_out1length' %scope], state_is_tuple = True)
+			if config.get(scope, 'link_type') == 'static_rnn':
+				model['%s_rnn' %scope] = getattr(tf.contrib.rnn, config.get(scope, 'cell_type'))(model['%s_out1length' %scope], state_is_tuple = True)
+			elif config.get(scope, 'link_type') == 'static_bidirectional_rnn':
+				model['%s_rnn_fw' %scope] = getattr(tf.contrib.rnn, config.get(scope, 'cell_type'))(model['%s_out1length' %scope], state_is_tuple = True)
+				model['%s_rnn_bw' %scope] = getattr(tf.contrib.rnn, config.get(scope, 'cell_type'))(model['%s_out1length' %scope], state_is_tuple = True)
 
 		with tf.variable_scope('stacks'), tf.name_scope('stacks'):
-			if config.get(scope, 'link_type') == 'rnn':
-				model['%s_rnns' %scope] = tf.nn.rnn_cell.MultiRNNCell([model['%s_rnn' %scope]] * config.getint(scope, 'layer_size'), state_is_tuple = True)
-			elif config.get(scope, 'link_type') == 'bidirectional_rnn':
-				model['%s_rnns_fw' %scope] = tf.nn.rnn_cell.MultiRNNCell([model['%s_rnn_fw' %scope]] * config.getint(scope, 'layer_size'), state_is_tuple = True)
-				model['%s_rnns_bw' %scope] = tf.nn.rnn_cell.MultiRNNCell([model['%s_rnn_bw' %scope]] * config.getint(scope, 'layer_size'), state_is_tuple = True)
+			if config.get(scope, 'link_type') == 'static_rnn':
+				model['%s_rnns' %scope] = tf.contrib.rnn.MultiRNNCell([model['%s_rnn' %scope]] * config.getint(scope, 'layer_size'), state_is_tuple = True)
+			elif config.get(scope, 'link_type') == 'static_bidirectional_rnn':
+				model['%s_rnns_fw' %scope] = tf.contrib.rnn.MultiRNNCell([model['%s_rnn_fw' %scope]] * config.getint(scope, 'layer_size'), state_is_tuple = True)
+				model['%s_rnns_bw' %scope] = tf.contrib.rnn.MultiRNNCell([model['%s_rnn_bw' %scope]] * config.getint(scope, 'layer_size'), state_is_tuple = True)
 
 		with tf.variable_scope('outputs'), tf.name_scope('outputs'):
-			if config.get(scope, 'link_type') == 'rnn':
-				outputs, state = getattr(tf.nn, config.get(scope, 'link_type'))(model['%s_rnns' %scope], model['%s_inputs' %scope], dtype = tf.float32, sequence_length = model['%s_in2length' %scope])
-				model['%s_outputs' %scope] = tf.pack(outputs, 0, '%s_outputs' %scope)
+			if config.get(scope, 'link_type') == 'static_rnn':
+				outputs, state = getattr(tf.contrib.rnn, config.get(scope, 'link_type'))(model['%s_rnns' %scope], model['%s_inputs' %scope], dtype = tf.float32, sequence_length = model['%s_in2length' %scope])
+				model['%s_outputs' %scope] = tf.stack(outputs, 0, '%s_outputs' %scope)
 				model['%s_state' %scope] = state[0]
-			elif config.get(scope, 'link_type') == 'bidirectional_rnn':
-				outputs, state_fw, state_bw = getattr(tf.nn, config.get(scope, 'link_type'))(cell_fw = model['%s_rnns_fw' %scope], cell_bw = model['%s_rnns_bw' %scope], inputs = model['%s_inputs' %scope], dtype = tf.float32, sequence_length = model['%s_in2length' %scope])
-				model['%s_outputs' %scope] = tf.pack(outputs, 0, '%s_outputs' %scope)
-				model['%s_state_c' %scope] = tf.concat(1, [state_fw[0][0], state_bw[0][0]], '%s_state_c' %scope)
-				model['%s_state_h' %scope] = tf.concat(1, [state_fw[0][1], state_bw[0][1]], '%s_state_h' %scope)
+			elif config.get(scope, 'link_type') == 'static_bidirectional_rnn':
+				outputs, state_fw, state_bw = getattr(tf.contrib.rnn, config.get(scope, 'link_type'))(cell_fw = model['%s_rnns_fw' %scope], cell_bw = model['%s_rnns_bw' %scope], inputs = model['%s_inputs' %scope], dtype = tf.float32, sequence_length = model['%s_in2length' %scope])
+				model['%s_outputs' %scope] = tf.stack(outputs, 0, '%s_outputs' %scope)
+				model['%s_state_c' %scope] = tf.concat([state_fw[0][0], state_bw[0][0]], 1, '%s_state_c' %scope)
+				model['%s_state_h' %scope] = tf.concat([state_fw[0][1], state_bw[0][1]], 1, '%s_state_h' %scope)
 				model['%s_state' %scope] = (model['%s_state_c' %scope], model['%s_state_h' %scope])
 
-			if config.get(scope, 'link_type') == 'bidirectional_rnn':
+			if config.get(scope, 'link_type') == 'static_bidirectional_rnn':
 				model['%s_out1length' %scope] *= 2
 
 	return model
