819ec193f74988c25b778be3eea9e330aa573fec
Amitrajit Sarkar <aaiijmrtt@gmail.com>
2017-03-09 10:38:34 +0530

    tensorflow 1.0 upgrade: dsi

--- code/dsi.py
+++ code/dsi.py
@@ -1,4 +1,4 @@
-import configparser, sys, os
+import configparser, sys, os, datetime, signal
 import tensorflow as tf, numpy as np
 import fnn, rnn, cnn, cei, speakerutil
 
@@ -22,13 +22,30 @@ def feed(features, labelslen, labelsind, labelsval, batch_size, time_size):
 	feed_dict.update({model['cnn_in2length']: [features[i].shape[1] for i in xrange(batch_size)], model['cei_labels_len']: labelslen, model['cei_labels_ind']: labelsind, model['cei_labels_val']: labelsval})
 	return feed_dict
 
+def handler(signum, stack):
+	print datetime.datetime.now(), 'saving model prematurely'
+	tf.train.Saver().save(sess, os.path.join(config.get('global', 'path'), 'model'))
+	sys.exit()
+
 if __name__ == '__main__':
 	config = configparser.ConfigParser(interpolation = configparser.ExtendedInterpolation())
 	config.read(sys.argv[1])
+	print datetime.datetime.now(), 'creating model'
 	model = dsi(config)
 
 	with tf.Session() as sess:
-		sess.run(tf.initialize_all_variables())
-		speakerutil.train(model, sess, config, sys.argv[2], feed)
-		print speakerutil.test(model, sess, config, sys.argv[2], feed)
+		signal.signal(signal.SIGINT, handler)
+		if sys.argv[2] == 'init':
+			sess.run(tf.global_variables_initializer())
+		elif sys.argv[2] == 'train':
+			tf.train.Saver().restore(sess, os.path.join(config.get('global', 'path'), 'model'))
+			print datetime.datetime.now(), 'training model'
+			loss = speechutil.train(model, sess, config, sys.argv[3], feed)
+			print datetime.datetime.now(), 'trained model', loss
+		elif sys.argv[2] == 'test':
+			tf.train.Saver().restore(sess, os.path.join(config.get('global', 'path'), 'model'))
+			print datetime.datetime.now(), 'testing model'
+			loss = speechutil.test(model, sess, config, sys.argv[3], feed)
+			print datetime.datetime.now(), 'tested model', loss
+		print datetime.datetime.now(), 'saving model'
 		tf.train.Saver().save(sess, os.path.join(config.get('global', 'path'), 'model'))
